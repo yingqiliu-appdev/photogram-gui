544d010fcd58d7144094b6c3eef163b62db0ead9
Jelani Woods <jelani@firstdraft.com>
2020-02-07 14:19:32 -0600

    Winter updates

--- Gemfile
+++ Gemfile
@@ -1,71 +1,52 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.3'
+ruby '2.6.5'
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.0'
-# Use sqlite3 as the database for Active Record
-
-# Use Puma as the app server
 gem 'puma', '~> 3.11'
-# Use SCSS for stylesheets
 gem 'sass-rails', '~> 5'
-# Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
-gem 'webpacker', '~> 4.0'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
-# Use Active Model has_secure_password
-gem 'bcrypt'
+gem 'bcrypt', '~> 3.1.7'
 
-# Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
-
-# Reduces boot times through caching; required in config/boot.rb
+gem 'activeadmin', '2.2.0'
+gem 'devise'
+gem 'http'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-end
-
-group :development do
-  # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'web-console', '>= 3.3.0'
-  gem 'listen', '>= 3.0.5', '< 3.2'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
-end
-
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
-group :development, :test do
   gem 'awesome_print'
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'dotenv-rails'
+  gem 'faker'
   gem 'grade_runner', github: 'firstdraft/grade_runner'
   gem 'pry-rails'
-  gem 'sqlite3', '~> 1.4.1'
+  gem 'sqlite3', '~> 1.4'
   gem 'table_print'
   gem 'web_git', github: 'firstdraft/web_git'
 end
 
 group :development do
-  gem 'annotate', '< 3.0.0'
+  gem 'annotate', '< 3.0.0' 
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'draft_generators', github: 'firstdraft/draft_generators'
   gem 'letter_opener'
+  gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'meta_request'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'tty-spinner'
+  gem 'web-console', '>= 3.3.0'
 end
 
 group :test do
-  gem 'capybara'
-  gem 'factory_bot_rails'
-  gem 'rspec-rails'
-  gem 'webmock'
+  gem 'capybara', '>= 2.15'
   gem 'rspec-html-matchers'
+  gem 'rspec-rails'
+  gem 'selenium-webdriver'
+  gem 'webdrivers'
+  gem 'webmock'
 end
 
 group :production do
@@ -73,5 +54,4 @@ group :production do
   gem 'rails_12factor'
 end
 
-gem 'devise'
-gem 'activeadmin', '2.2.0'
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -3,74 +3,74 @@ Rails.application.routes.draw do
   # User routes
 
   # CREATE
-  match("/insert_user_record", {:controller => "users", :action => "create", :via => "get"})
+  get("/insert_user_record", {:controller => "users", :action => "create" })
 
   # READ
-  match("/users", {:controller => "users", :action => "index", :via => "get"})
-  match("/users/:the_username", {:controller => "users", :action => "show", :via => "get"})
-  match("/users/:the_username/own_photos", {:controller => "users", :action => "own_photos", :via => "get"})
-  match("/users/:the_username/liked_photos", {:controller => "users", :action => "liked_photos", :via => "get"})
-  match("/users/:the_username/feed", {:controller => "users", :action => "feed", :via => "get"})
-  match("/users/:the_username/discover", {:controller => "users", :action => "discover", :via => "get"})
+  get("/users", {:controller => "users", :action => "index" })
+  get("/users/:the_username", {:controller => "users", :action => "show" })
+  get("/users/:the_username/own_photos", {:controller => "users", :action => "own_photos" })
+  get("/users/:the_username/liked_photos", {:controller => "users", :action => "liked_photos" })
+  get("/users/:the_username/feed", {:controller => "users", :action => "feed" })
+  get("/users/:the_username/discover", {:controller => "users", :action => "discover" })
 
 
   # UPDATE
-  match("/update_user/:the_user_id", {:controller => "users", :action => "update", :via => "get"})
+  get("/update_user/:the_user_id", {:controller => "users", :action => "update" })
 
   # DELETE
-  match("/delete_user/:the_user_id", {:controller => "users", :action => "destroy", :via => "get"})
+  get("/delete_user/:the_user_id", {:controller => "users", :action => "destroy" })
 
 
   # Photo routes
 
   # CREATE
-  match("/insert_photo_record", { :controller => "photos", :action => "create", :via => "get"})
+  get("/insert_photo_record", { :controller => "photos", :action => "create" })
 
   # READ
-  match("/photos", { :controller => "photos", :action => "index", :via => "get"})
+  get("/photos", { :controller => "photos", :action => "index" })
 
-  match("/photos/:the_photo_id", { :controller => "photos", :action => "show", :via => "get"})
+  get("/photos/:the_photo_id", { :controller => "photos", :action => "show" })
 
-  match("/photos/:the_photo_id/comments", { :controller => "photos", :action => "comments", :via => "get"})
-  match("/photos/:the_photo_id/likes", { :controller => "photos", :action => "likes", :via => "get"})
-  match("/photos/:the_photo_id/fans", { :controller => "photos", :action => "fans", :via => "get"})
+  get("/photos/:the_photo_id/comments", { :controller => "photos", :action => "comments" })
+  get("/photos/:the_photo_id/likes", { :controller => "photos", :action => "likes" })
+  get("/photos/:the_photo_id/fans", { :controller => "photos", :action => "fans" })
 
   # UPDATE
-  match("/update_photo/:the_photo_id", { :controller => "photos", :action => "update", :via => "get"})
+  get("/update_photo/:the_photo_id", { :controller => "photos", :action => "update" })
 
   # DELETE
-  match("/delete_photo/:the_photo_id", { :controller => "photos", :action => "destroy", :via => "get"})
+  get("/delete_photo/:the_photo_id", { :controller => "photos", :action => "destroy" })
 
   # Like routes
 
   # CREATE
-  match("/insert_like_record", {:controller => "likes", :action => "create", :via => "get"})
+  get("/insert_like_record", {:controller => "likes", :action => "create" })
 
   # READ
-  match("/likes", {:controller => "likes", :action => "index", :via => "get"})
-  match("/likes/:the_like_id", {:controller => "likes", :action => "show", :via => "get"})
+  get("/likes", {:controller => "likes", :action => "index" })
+  get("/likes/:the_like_id", {:controller => "likes", :action => "show" })
 
   # UPDATE
-  match("/update_like/:the_like_id", {:controller => "likes", :action => "update", :via => "get"})
+  get("/update_like/:the_like_id", {:controller => "likes", :action => "update" })
 
   # DELETE
-  match("/delete_like/:the_like_id", {:controller => "likes", :action => "destroy", :via => "get"})
+  get("/delete_like/:the_like_id", {:controller => "likes", :action => "destroy" })
 
   # Comment routes
 
   # CREATE
-  match("/insert_comment_record", { :controller => "comments", :action => "create", :via => "get"})
+  get("/insert_comment_record", { :controller => "comments", :action => "create" })
 
   # READ
-  match("/comments", { :controller => "comments", :action => "index", :via => "get"})
-  match("/comments/:the_comment_id", { :controller => "comments", :action => "show", :via => "get"})
+  get("/comments", { :controller => "comments", :action => "index" })
+  get("/comments/:the_comment_id", { :controller => "comments", :action => "show" })
 
   # UPDATE
-  match("/update_comment_record/:the_comment_id", { :controller => "comments", :action => "update", :via => "get"})
+  get("/update_comment_record/:the_comment_id", { :controller => "comments", :action => "update" })
 
   # DELETE
 
-  match("/delete_comment/:the_comment_id", { :controller => "comments", :action => "destroy", :via => "get"})
+  get("/delete_comment/:the_comment_id", { :controller => "comments", :action => "destroy" })
 
   # ======== Routes for Admin Dashboard =============
   devise_for :admin_users, ActiveAdmin::Devise.config
