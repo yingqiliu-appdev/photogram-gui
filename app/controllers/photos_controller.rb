bba0949889096d7d94eee14f61b7c64caa3b8fd0
yingqi0408 <ying_liu@chicagobooth.edu>
2021-11-08 18:37:34 +0000

    Monday before Investments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,21 @@ class PhotosController < ApplicationController
 
   end
 
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id})
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photo_templates/show.html.erb"})
+  end
+
+  def baii
+
+    the_id = params.fetch("toast_id")
+    matching_photos = Photo.where ({ :id => the_id})
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+    # render({ :template => "photo_template/baii.html.erb"})
+    redirect_to("/photos")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get("/users/:username", { :controller => "users", :action => "show"})
 
   get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+
+    get("delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
 end
